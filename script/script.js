49a59471797905ca41c0ef48ef34f11363bf2e6b
Lahari <laharidonthula@hotmail.com>
2024-09-04 18:05:06 +0530

    created

---
+++ script/script.js
@@ -0,0 +1,21 @@
+// // console.log("Jai Mahishmathi")
+// const student = {
+//     name:"lahari",
+//     age:23,
+//     cgpa:7.4,
+//     isPass: true,
+// };
+// student['age'] =student['age'] + 2;
+// console.log(student["age"]);
+
+// student['name'] = 'rahul';
+// console.log(student.name);
+
+const profile = {
+    username : "donthula lahari",
+    isFollow : true,
+    followers : 250,
+    following : 2,
+}
+
+console.log(profile);
